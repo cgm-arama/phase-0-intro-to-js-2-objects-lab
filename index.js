1ead12fe9df6b22919867ac1df49d48d4eb18187
Eric <eric.arama@student.moringaschool.com>
2023-03-20 10:52:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  }
+  
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+
+  const employee = {
+    name: 'Eric',
+    age: 30,
+    jobTitle: 'Software Engineer',
+  };
+  
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'age', 18);
+  console.log(updatedEmployee);
+  
+
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, 'age', 18);
+  console.log(employee);
+  
+  const newEmployee = deleteFromEmployeeByKey(employee, 'jobTitle');
+  console.log(newEmployee);
+  
+  destructivelyDeleteFromEmployeeByKey(employee, 'jobTitle');
+  console.log(employee);
+  
+ 
+{
+    name: 'Eric';
+    age: 18;
+    jobTitle: 'Software Engineer';
+}
+  
+
+  {
+    name: 'Eric';
+    age: 18;
+    jobTitle: 'Software Engineer';
+  }
+  
+
+  {
+    name: 'Eric';
+    age : 18;
+  }
+  
+
+  {
+    name: 'Eric';
+    age: 18;
+  }
+   
+  
